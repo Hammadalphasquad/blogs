d6870fd1ca0bd4a520f8609cb4af8cccc73bd92e
Hammadalphasquad <hammad.alphasquad@gmail.com>
2023-07-14 15:11:57 +0500

    hamb

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -75,21 +75,20 @@
         </router-link>
         <!-- Mobile menu button -->
         <div @click="toggleNav" class="flex md:hidden">
-          <button
-            type="button"
-            class="
-              text-gray-100
-              hover:text-gray-400
-              focus:outline-none focus:text-gray-400
-            "
-          >
-            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-              <path
-                fill-rule="evenodd"
-                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-              ></path>
-            </svg>
-          </button>
+          <button class="relative group">
+        <div class="relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-slate-700 ring-0 ring-gray-300 hover:ring-8 group-focus:ring-4 ring-opacity-30 duration-200 shadow-md">
+          <div class="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center overflow-hidden">
+            <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-x-10"></div>
+            <div class="bg-white h-[2px] w-7 rounded transform transition-all duration-300 group-focus:translate-x-10 delay-75"></div>
+            <div class="bg-white h-[2px] w-7 transform transition-all duration-300 origin-left group-focus:translate-x-10 delay-150"></div>
+
+            <div class="absolute items-center justify-between transform transition-all duration-500 top-2.5 -translate-x-10 group-focus:translate-x-0 flex w-0 group-focus:w-12">
+              <div class="absolute bg-white h-[2px] w-5 transform transition-all duration-500 rotate-0 delay-300 group-focus:rotate-45"></div>
+              <div class="absolute bg-white h-[2px] w-5 transform transition-all duration-500 -rotate-0 delay-300 group-focus:-rotate-45"></div>
+            </div>
+          </div>
+        </div>
+      </button>
         </div>
       </div>
 
@@ -104,11 +103,11 @@
         "
       >
       <router-link to="/"><li class="text-white cursor-pointer">Home</li></router-link>
-        <li class="text-gray-100 hover:text-indigo-400">About us</li>
-        <li class="text-gray-100 hover:text-indigo-400">Features</li>
-        <li class="text-gray-100 hover:text-indigo-400">Pricing</li>
-        <li class="text-gray-100 hover:text-indigo-400">FAQ</li>
-        <li class="text-gray-100 hover:text-indigo-400">Blog</li>
+        <li class="text-gray-100 hover:text-indigo-400 cursor-pointer">About us</li>
+        <li class="text-gray-100 hover:text-indigo-400 cursor-pointer">Features</li>
+        <li class="text-gray-100 hover:text-indigo-400 cursor-pointer">Pricing</li>
+        <li class="text-gray-100 hover:text-indigo-400 cursor-pointer">FAQ</li>
+        <li class="text-gray-100 hover:text-indigo-400 cursor-pointer">Blog</li>
       </ul>
     </nav>
   </div>
